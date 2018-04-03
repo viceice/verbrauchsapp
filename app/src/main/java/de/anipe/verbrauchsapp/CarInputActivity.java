d699113e528e45f08437cf8e3ff4714fba385f10
Michael Kriese <viceice@users.noreply.github.com>
2018-04-03 12:00:20 +0200

    Use toolbar buttons.

--- app/src/main/java/de/anipe/verbrauchsapp/CarInputActivity.java
+++ app/src/main/java/de/anipe/verbrauchsapp/CarInputActivity.java
@@ -4,11 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -26,206 +25,206 @@ import de.anipe.verbrauchsapp.objects.Fueltype;
  */
 public class CarInputActivity extends AppCompatActivity {
 
-	private ConsumptionDataSource dataSource;
-	private FileSystemAccessor accessor;
-	private long carId;
-	private boolean update = false;
+    private ConsumptionDataSource dataSource;
+    private FileSystemAccessor accessor;
+    private long carId;
+    private boolean update = false;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		Intent intent = getIntent();
-		Bundle bundle = intent.getExtras();
-		if (bundle != null) {
-			carId = bundle.getLong("carid");
-			update = bundle.getBoolean("update");
-		}
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            carId = bundle.getLong("carid");
+            update = bundle.getBoolean("update");
+        }
 
-		setContentView(R.layout.inputform_car);
+        setContentView(R.layout.inputform_car);
 
         Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
 
-		dataSource = ConsumptionDataSource.getInstance(this);
-		try {
-			dataSource.open();
-		} catch (SQLException e) {
-			Toast.makeText(CarInputActivity.this,
-					"Fehler beim Öffnen der Datenbank!", Toast.LENGTH_LONG)
-					.show();
-		}
+        dataSource = ConsumptionDataSource.getInstance(this);
+        try {
+            dataSource.open();
+        } catch (SQLException e) {
+            Toast.makeText(CarInputActivity.this,
+                "Fehler beim Öffnen der Datenbank!", Toast.LENGTH_LONG)
+                .show();
+        }
 
-		accessor = FileSystemAccessor.getInstance();
+        accessor = FileSystemAccessor.getInstance();
 
-		Spinner fuelTypeSpinner = findViewById(R.id.fueltypes_spinner);
-		ArrayAdapter<CharSequence> fuelAdapter = ArrayAdapter
-				.createFromResource(this, R.array.fuel_types,
-						android.R.layout.simple_spinner_item);
-		// Specify the layout to use when the list of choices appears
-		fuelAdapter
-				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		// Apply the adapter to the spinner
-		fuelTypeSpinner.setAdapter(fuelAdapter);
+        Spinner fuelTypeSpinner = findViewById(R.id.fueltypes_spinner);
+        ArrayAdapter<CharSequence> fuelAdapter = ArrayAdapter
+            .createFromResource(this, R.array.fuel_types,
+                android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        fuelAdapter
+            .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        fuelTypeSpinner.setAdapter(fuelAdapter);
 
-		Spinner brandSpinner = findViewById(R.id.brand_spinner);
-		ArrayAdapter<CharSequence> brandAdapter = ArrayAdapter
-				.createFromResource(this, R.array.brands,
-						android.R.layout.simple_spinner_item);
-		// Specify the layout to use when the list of choices appears
-		brandAdapter
-				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		// Apply the adapter to the spinner
-		brandSpinner.setAdapter(brandAdapter);
+        Spinner brandSpinner = findViewById(R.id.brand_spinner);
+        ArrayAdapter<CharSequence> brandAdapter = ArrayAdapter
+            .createFromResource(this, R.array.brands,
+                android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        brandAdapter
+            .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        brandSpinner.setAdapter(brandAdapter);
 
-		if (update) {
-			Car car = dataSource.getCarForId(carId);
+        if (update) {
+            Car car = dataSource.getCarForId(carId);
 
-			((EditText) findViewById(R.id.car_type_input)).setText(car
-					.getType());
+            ((EditText) findViewById(R.id.car_type_input)).setText(car
+                .getType());
 
-			int brandPosition = brandAdapter
-					.getPosition(car.getBrand().value());
-			brandSpinner.setSelection(brandPosition);
+            int brandPosition = brandAdapter
+                .getPosition(car.getBrand().value());
+            brandSpinner.setSelection(brandPosition);
 
-			((EditText) findViewById(R.id.car_numberplate_input)).setText(car
-					.getNumberPlate());
+            ((EditText) findViewById(R.id.car_numberplate_input)).setText(car
+                .getNumberPlate());
 
-			((EditText) findViewById(R.id.car_startkm_input)).setText(String
-					.valueOf(car.getStartKm()));
+            ((EditText) findViewById(R.id.car_startkm_input)).setText(String
+                .valueOf(car.getStartKm()));
 
-			int spinnerPosition = fuelAdapter.getPosition(car.getFuelType()
-					.value());
-			fuelTypeSpinner.setSelection(spinnerPosition);
-		}
-
-		Button addButton = findViewById(R.id.button_add_car);
-		addButton.setOnClickListener(clickListener);
-
-		if (update) {
-			addButton.setText("Datensatz speichern");
-		}
-
+            int spinnerPosition = fuelAdapter.getPosition(car.getFuelType()
+                .value());
+            fuelTypeSpinner.setSelection(spinnerPosition);
+        }
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-	}
+    }
 
-	@Override
-	protected void onPause() {
-		dataSource.close();
-		super.onPause();
-	}
+    @Override
+    protected void onPause() {
+        dataSource.close();
+        super.onPause();
+    }
 
-	@Override
-	protected void onResume() {
-		try {
-			dataSource.open();
-		} catch (SQLException e) {
-			Toast.makeText(CarInputActivity.this,
-					"Fehler beim Öffnen der Datenbank!", Toast.LENGTH_LONG)
-					.show();
-		}
-		super.onResume();
-	}
+    @Override
+    protected void onResume() {
+        try {
+            dataSource.open();
+        } catch (SQLException e) {
+            Toast.makeText(CarInputActivity.this,
+                "Fehler beim Öffnen der Datenbank!", Toast.LENGTH_LONG)
+                .show();
+        }
+        super.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.car_input, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
+                return true;
+            case R.id.action_done:
+                done();
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-	// Create an anonymous implementation of OnClickListener
-	private OnClickListener clickListener = new OnClickListener() {
-		public void onClick(View v) {
-			if (verify()) {
-				Car car = new Car();
-				car.setType(((EditText) findViewById(R.id.car_type_input))
-						.getText().toString());
-				car.setBrand(Brand
-						.fromValue(((Spinner) findViewById(R.id.brand_spinner))
-								.getSelectedItem().toString()));
-				car.setNumberPlate(((EditText) findViewById(R.id.car_numberplate_input))
-						.getText().toString());
-				car.setStartKm(Integer
-						.parseInt(((EditText) findViewById(R.id.car_startkm_input))
-								.getText().toString()));
-				car.setFuelType(Fueltype
-						.fromValue(((Spinner) findViewById(R.id.fueltypes_spinner))
-								.getSelectedItem().toString()));
-				car.setIcon(accessor.getBitmapForBrand(getApplicationContext(),
-						car.getBrand()));
+    private void done() {
+        if (verify()) {
+            Car car = new Car();
+            car.setType(((EditText) findViewById(R.id.car_type_input))
+                .getText().toString());
+            car.setBrand(Brand
+                .fromValue(((Spinner) findViewById(R.id.brand_spinner))
+                    .getSelectedItem().toString()));
+            car.setNumberPlate(((EditText) findViewById(R.id.car_numberplate_input))
+                .getText().toString());
+            car.setStartKm(Integer
+                .parseInt(((EditText) findViewById(R.id.car_startkm_input))
+                    .getText().toString()));
+            car.setFuelType(Fueltype
+                .fromValue(((Spinner) findViewById(R.id.fueltypes_spinner))
+                    .getSelectedItem().toString()));
+            car.setIcon(accessor.getBitmapForBrand(getApplicationContext(),
+                car.getBrand()));
 
-				if (update) {
-					car.setCarId(carId);
-					car.setImage(dataSource.getImageForCarId(carId));
-					if (dataSource.updateCar(car) > 0) {
-						Toast.makeText(CarInputActivity.this,
-								"Fahrzeugdatensatz geändert!",
-								Toast.LENGTH_LONG).show();
-						finish();
-					} else {
-						Toast.makeText(
-								CarInputActivity.this,
-								"Fehler beim Speichern des Fahrzeug-Datensatzes!",
-								Toast.LENGTH_LONG).show();
-					}
-				} else {
-					if (dataSource.addCar(car) > -1) {
-						Toast.makeText(CarInputActivity.this,
-								"Fahrzeug hinzugefügt!", Toast.LENGTH_LONG)
-								.show();
-						finish();
-					} else {
-						Toast.makeText(
-								CarInputActivity.this,
-								"Fehler beim Hinzufügen des Fahrzeug-Datensatzes!",
-								Toast.LENGTH_LONG).show();
-					}
-				}
-			}
-		}
+            if (update) {
+                car.setCarId(carId);
+                car.setImage(dataSource.getImageForCarId(carId));
+                if (dataSource.updateCar(car) > 0) {
+                    Toast.makeText(CarInputActivity.this,
+                        "Fahrzeugdatensatz geändert!",
+                        Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    Toast.makeText(
+                        CarInputActivity.this,
+                        "Fehler beim Speichern des Fahrzeug-Datensatzes!",
+                        Toast.LENGTH_LONG).show();
+                }
+            } else {
+                if (dataSource.addCar(car) > -1) {
+                    Toast.makeText(CarInputActivity.this,
+                        "Fahrzeug hinzugefügt!", Toast.LENGTH_LONG)
+                        .show();
+                    finish();
+                } else {
+                    Toast.makeText(
+                        CarInputActivity.this,
+                        "Fehler beim Hinzufügen des Fahrzeug-Datensatzes!",
+                        Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
 
-		private boolean verify() {
-			String carType = ((EditText) findViewById(R.id.car_type_input))
-					.getText().toString();
-			if (carType.equals("")) {
-				Toast.makeText(CarInputActivity.this,
-						"Fahrzeugtyp darf nicht leer sein!", Toast.LENGTH_LONG)
-						.show();
-				return false;
-			}
-			String carPlate = ((EditText) findViewById(R.id.car_numberplate_input))
-					.getText().toString();
-			if (carPlate.equals("")) {
-				Toast.makeText(CarInputActivity.this,
-						"Kennzeichen darf nicht leer sein!", Toast.LENGTH_LONG)
-						.show();
-				return false;
-			}
-			String startKm = ((EditText) findViewById(R.id.car_startkm_input))
-					.getText().toString();
-			if (startKm.equals("")) {
-				Toast.makeText(CarInputActivity.this,
-						"Start-Kilometer darf nicht leer sein!",
-						Toast.LENGTH_LONG).show();
-				return false;
-			}
-			try {
-				Integer.parseInt(startKm);
-			} catch (NumberFormatException nfe) {
-				Toast.makeText(
-						CarInputActivity.this,
-						"Start-Kilometer muss ein numerischer, ganzzahliger Wert sein!",
-						Toast.LENGTH_LONG).show();
-				return false;
-			}
+    private boolean verify() {
+        String carType = ((EditText) findViewById(R.id.car_type_input))
+            .getText().toString();
+        if (carType.equals("")) {
+            Toast.makeText(CarInputActivity.this,
+                "Fahrzeugtyp darf nicht leer sein!", Toast.LENGTH_LONG)
+                .show();
+            return false;
+        }
+        String carPlate = ((EditText) findViewById(R.id.car_numberplate_input))
+            .getText().toString();
+        if (carPlate.equals("")) {
+            Toast.makeText(CarInputActivity.this,
+                "Kennzeichen darf nicht leer sein!", Toast.LENGTH_LONG)
+                .show();
+            return false;
+        }
+        String startKm = ((EditText) findViewById(R.id.car_startkm_input))
+            .getText().toString();
+        if (startKm.equals("")) {
+            Toast.makeText(CarInputActivity.this,
+                "Start-Kilometer darf nicht leer sein!",
+                Toast.LENGTH_LONG).show();
+            return false;
+        }
+        try {
+            Integer.parseInt(startKm);
+        } catch (NumberFormatException nfe) {
+            Toast.makeText(
+                CarInputActivity.this,
+                "Start-Kilometer muss ein numerischer, ganzzahliger Wert sein!",
+                Toast.LENGTH_LONG).show();
+            return false;
+        }
 
-			return true;
-		}
-	};
-
+        return true;
+    }
 }
+
--- app/src/main/java/de/anipe/verbrauchsapp/ConsumptionInputActivity.java
+++ app/src/main/java/de/anipe/verbrauchsapp/ConsumptionInputActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -25,194 +27,83 @@ import de.anipe.verbrauchsapp.objects.Consumption;
  */
 public class ConsumptionInputActivity extends AppCompatActivity implements DatePickerFragment.OnFragmentInteractionListener {
 
-	private ConsumptionDataSource dataSource;
-	private long carId;
-	private int kmState;
-	final int Date_Dialog_ID = 19999;
+    private ConsumptionDataSource dataSource;
+    private long carId;
+    private int kmState;
+    final int Date_Dialog_ID = 19999;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		Intent intent = getIntent();
-		Bundle bundle = intent.getExtras();
-		carId = bundle.getLong("carid");
-		kmState = bundle.getInt("kmstate");
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        carId = bundle.getLong("carid");
+        kmState = bundle.getInt("kmstate");
 
-		setContentView(R.layout.inputform_consumption);
+        setContentView(R.layout.inputform_consumption);
 
         Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
 
-		dataSource = ConsumptionDataSource.getInstance(this);
-		try {
-			dataSource.open();
-		} catch (SQLException e) {
-			Toast.makeText(ConsumptionInputActivity.this,
-					"Fehler beim Öffnen der Datenbank!", Toast.LENGTH_LONG)
-					.show();
-		}
-
-		EditText dateText = findViewById(R.id.datumTextLine);
-		final Calendar c = Calendar.getInstance();
-		dateText.setText(c.get(Calendar.DAY_OF_MONTH) + "."
-				+ (c.get(Calendar.MONTH) + 1) + "." + c.get(Calendar.YEAR));
-		dateText.setOnClickListener(clickListener);
-
-		Button addButton = findViewById(R.id.button_add_consumption);
-		addButton.setOnClickListener(clickListener);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-	}
-
-	@Override
-	protected void onPause() {
-		dataSource.close();
-		super.onPause();
-	}
-
-	@Override
-	protected void onResume() {
-		try {
-			dataSource.open();
-		} catch (SQLException e) {
-			Toast.makeText(ConsumptionInputActivity.this,
-					"Fehler beim Öffnen der Datenbank!", Toast.LENGTH_LONG)
-					.show();
-		}
-		super.onResume();
-	}
-
-	// Create an anonymous implementation of OnClickListener
-	private OnClickListener clickListener = new OnClickListener() {
-		@SuppressWarnings("deprecation")
-		public void onClick(View v) {
-			switch (v.getId()) {
-			case R.id.datumTextLine:
-				// showDialog(Date_Dialog_ID);
-                DialogFragment f = DatePickerFragment.newInstance(getCalendarFromDateText(((EditText) findViewById(R.id.datumTextLine))
-                    .getText().toString()));
-                f.show(getSupportFragmentManager(), "datePicker");
-				break;
-			case R.id.button_add_consumption:
-				if (verify()) {
-					Consumption cons = new Consumption();
-					cons.setCarId(carId);
-					cons.setDate(getDateFromDateText(((EditText) findViewById(R.id.datumTextLine))
-							.getText().toString()));
-					cons.setRefuelmileage(Integer
-							.parseInt(((EditText) findViewById(R.id.cons_km_input))
-									.getText().toString()));
-					cons.setDrivenmileage(cons.getRefuelmileage() - kmState);
-					cons.setRefuelliters(Double
-							.parseDouble(((EditText) findViewById(R.id.cons_liter_input))
-									.getText().toString().replace(',', '.')));
-					String priceString = ((EditText) findViewById(R.id.cons_price_input))
-							.getText().toString().replace(',', '.');
-					cons.setRefuelprice(priceString.equals("") ? 0 : Double
-							.parseDouble(priceString));
-					cons.setConsumption(calculateConsumption(
-							cons.getRefuelliters(), cons.getDrivenmileage()));
-
-					if (dataSource.addConsumption(cons) > -1) {
-						Toast.makeText(ConsumptionInputActivity.this,
-								"Verbrauchsdatensatz hinzugefügt!",
-								Toast.LENGTH_LONG).show();
-						finish();
-					} else {
-						Toast.makeText(
-								ConsumptionInputActivity.this,
-								"Fehler beim Hinzufügen des Verbrauchsdatensatzes!",
-								Toast.LENGTH_LONG).show();
-					}
-					finish();
-				}
-				break;
-			}
-
-		}
-
-		private boolean verify() {
-			String kmState = ((EditText) findViewById(R.id.cons_km_input))
-					.getText().toString();
-			if (kmState.equals("")) {
-				Toast.makeText(ConsumptionInputActivity.this,
-						"Kilometerstand darf nicht leer sein!",
-						Toast.LENGTH_LONG).show();
-				return false;
-			}
-			try {
-				int val = Integer.parseInt(kmState);
-				if (val - ConsumptionInputActivity.this.kmState <= 0) {
-					Toast.makeText(
-							ConsumptionInputActivity.this,
-							"Kilometerstand muss größer als der letzte Kilometerstand sein!",
-							Toast.LENGTH_LONG).show();
-					return false;
-				}
-			} catch (NumberFormatException nfe) {
-				Toast.makeText(ConsumptionInputActivity.this,
-						"Kilometerstand ganzzahlig eingeben!",
-						Toast.LENGTH_LONG).show();
-				return false;
-			}
-			String liter = ((EditText) findViewById(R.id.cons_liter_input))
-					.getText().toString().replace(',', '.');
-			if (liter.equals("")) {
-				Toast.makeText(ConsumptionInputActivity.this,
-						"Literzahl darf nicht leer sein!", Toast.LENGTH_LONG)
-						.show();
-				return false;
-			}
-			try {
-				Double.parseDouble(liter);
-			} catch (NumberFormatException nfe) {
-				Toast.makeText(
-						ConsumptionInputActivity.this,
-						"Literzahl ggf. mit Komma oder Punkt separiert eingeben!",
-						Toast.LENGTH_LONG).show();
-				return false;
-			}
-			String price = ((EditText) findViewById(R.id.cons_price_input))
-					.getText().toString().replace(',', '.');
-			if (!price.equals("")) {
-				try {
-					Double.parseDouble(price);
-				} catch (NumberFormatException nfe) {
-					Toast.makeText(
-							ConsumptionInputActivity.this,
-							"Preis ggf. mit Komma oder Punkt separiert eingeben!",
-							Toast.LENGTH_LONG).show();
-					return false;
-				}
-			}
-
-			return true;
-		}
-
-        private Calendar getCalendarFromDateText(String text) {
-            Calendar cal = Calendar.getInstance();
-            String[] date = text.split("\\.");
-            cal.set(Integer.parseInt(date[2]), Integer.parseInt(date[1]) - 1,
-                Integer.parseInt(date[0]));
-
-            return cal;
+        dataSource = ConsumptionDataSource.getInstance(this);
+        try {
+            dataSource.open();
+        } catch (SQLException e) {
+            Toast.makeText(ConsumptionInputActivity.this,
+                "Fehler beim Öffnen der Datenbank!", Toast.LENGTH_LONG)
+                .show();
         }
 
-		private Date getDateFromDateText(String text) {
-			return getCalendarFromDateText(text).getTime();
-		}
+        EditText dateText = findViewById(R.id.datumTextLine);
+        final Calendar c = Calendar.getInstance();
+        dateText.setText(c.get(Calendar.DAY_OF_MONTH) + "."
+            + (c.get(Calendar.MONTH) + 1) + "." + c.get(Calendar.YEAR));
+        dateText.setOnClickListener(v -> {
+            DialogFragment f = DatePickerFragment.newInstance(getCalendarFromDateText(((EditText) findViewById(R.id.datumTextLine))
+                .getText().toString()));
+            f.show(getSupportFragmentManager(), "datePicker");
+        });
 
-		private double calculateConsumption(double liters, int drivenKm) {
-			return ((liters * 100) / drivenKm);
-		}
-	};
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onPause() {
+        dataSource.close();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        try {
+            dataSource.open();
+        } catch (SQLException e) {
+            Toast.makeText(ConsumptionInputActivity.this,
+                "Fehler beim Öffnen der Datenbank!", Toast.LENGTH_LONG)
+                .show();
+        }
+        super.onResume();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.consumption_input, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 navigateUpTo(this.getParentActivityIntent());
+                return true;
+            case R.id.action_done:
+                done();
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -225,5 +116,116 @@ public class ConsumptionInputActivity extends AppCompatActivity implements DateP
             + String.valueOf(date.get(Calendar.MONTH) + 1) + "."
             + String.valueOf(date.get(Calendar.YEAR));
         dateText.setText(dateSelected);
+    }
+
+
+    private void done() {
+        if (verify()) {
+            Consumption cons = new Consumption();
+            cons.setCarId(carId);
+            cons.setDate(getDateFromDateText(((EditText) findViewById(R.id.datumTextLine))
+                .getText().toString()));
+            cons.setRefuelmileage(Integer
+                .parseInt(((EditText) findViewById(R.id.cons_km_input))
+                    .getText().toString()));
+            cons.setDrivenmileage(cons.getRefuelmileage() - kmState);
+            cons.setRefuelliters(Double
+                .parseDouble(((EditText) findViewById(R.id.cons_liter_input))
+                    .getText().toString().replace(',', '.')));
+            String priceString = ((EditText) findViewById(R.id.cons_price_input))
+                .getText().toString().replace(',', '.');
+            cons.setRefuelprice(priceString.equals("") ? 0 : Double
+                .parseDouble(priceString));
+            cons.setConsumption(calculateConsumption(
+                cons.getRefuelliters(), cons.getDrivenmileage()));
+
+            if (dataSource.addConsumption(cons) > -1) {
+                Toast.makeText(ConsumptionInputActivity.this,
+                    "Verbrauchsdatensatz hinzugefügt!",
+                    Toast.LENGTH_LONG).show();
+                finish();
+            } else {
+                Toast.makeText(
+                    ConsumptionInputActivity.this,
+                    "Fehler beim Hinzufügen des Verbrauchsdatensatzes!",
+                    Toast.LENGTH_LONG).show();
+            }
+            finish();
+        }
+    }
+
+    private boolean verify() {
+        String kmState = ((EditText) findViewById(R.id.cons_km_input))
+            .getText().toString();
+        if (kmState.equals("")) {
+            Toast.makeText(ConsumptionInputActivity.this,
+                "Kilometerstand darf nicht leer sein!",
+                Toast.LENGTH_LONG).show();
+            return false;
+        }
+        try {
+            int val = Integer.parseInt(kmState);
+            if (val - ConsumptionInputActivity.this.kmState <= 0) {
+                Toast.makeText(
+                    ConsumptionInputActivity.this,
+                    "Kilometerstand muss größer als der letzte Kilometerstand sein!",
+                    Toast.LENGTH_LONG).show();
+                return false;
+            }
+        } catch (NumberFormatException nfe) {
+            Toast.makeText(ConsumptionInputActivity.this,
+                "Kilometerstand ganzzahlig eingeben!",
+                Toast.LENGTH_LONG).show();
+            return false;
+        }
+        String liter = ((EditText) findViewById(R.id.cons_liter_input))
+            .getText().toString().replace(',', '.');
+        if (liter.equals("")) {
+            Toast.makeText(ConsumptionInputActivity.this,
+                "Literzahl darf nicht leer sein!", Toast.LENGTH_LONG)
+                .show();
+            return false;
+        }
+        try {
+            Double.parseDouble(liter);
+        } catch (NumberFormatException nfe) {
+            Toast.makeText(
+                ConsumptionInputActivity.this,
+                "Literzahl ggf. mit Komma oder Punkt separiert eingeben!",
+                Toast.LENGTH_LONG).show();
+            return false;
+        }
+        String price = ((EditText) findViewById(R.id.cons_price_input))
+            .getText().toString().replace(',', '.');
+        if (!price.equals("")) {
+            try {
+                Double.parseDouble(price);
+            } catch (NumberFormatException nfe) {
+                Toast.makeText(
+                    ConsumptionInputActivity.this,
+                    "Preis ggf. mit Komma oder Punkt separiert eingeben!",
+                    Toast.LENGTH_LONG).show();
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private Calendar getCalendarFromDateText(String text) {
+        Calendar cal = Calendar.getInstance();
+        String[] date = text.split("\\.");
+        cal.set(Integer.parseInt(date[2]), Integer.parseInt(date[1]) - 1,
+            Integer.parseInt(date[0]));
+
+        return cal;
+    }
+
+    private Date getDateFromDateText(String text) {
+        return getCalendarFromDateText(text).getTime();
+    }
+
+    private double calculateConsumption(double liters, int drivenKm) {
+        return ((liters * 100) / drivenKm);
     }
 }
